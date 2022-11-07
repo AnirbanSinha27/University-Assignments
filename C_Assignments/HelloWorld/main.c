7814210e981e09e4faf11315cac57131ba51a7a8
AnirbanSinha27 <107923453+AnirbanSinha27@users.noreply.github.com>
2022-11-07 14:47:45 +0530

    Initial Commit

---
+++ C_Assignments/HelloWorld/main.c
@@ -0,0 +1,14 @@
+/*
+Program to who hello world on the terminal
+
+Copyright (c) 2022 Anirban Sinha
+Licensed under MIT License
+*/
+
+#include <stdio.h>
+int main()
+{
+    printf("Hello world\n");
+    
+    return 0;
+}
