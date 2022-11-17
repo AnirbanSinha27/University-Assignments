96a01050ee71aaf069b5c0c59fa7836595413ee6
AnirbanSinha27 <107923453+AnirbanSinha27@users.noreply.github.com>
2022-11-17 14:16:03 +0530

    Dekho aaya hai kon

---
+++ C_Assignments/Palindrome/main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+int main(){
+    char arr[100];
+    printf("Enter the string \n");
+    gets( arr );
+    bool flag=1;
+    for(int i=0;i<strlen(arr);i++){
+        if(arr[i]!=arr[strlen(arr)-1-i]){
+            flag=0;
+            break;
+        }
+    }
+    if(flag==1){
+        printf("The word is a palindrome \n");
+    }
+    else{
+        printf("The word is mot a palindrome \n");
+    }
+    
+    return 0;
+}
