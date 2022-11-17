4cec240c13a708e5da33f8948432e975eab6c68d
AnirbanSinha27 <107923453+AnirbanSinha27@users.noreply.github.com>
2022-11-17 14:25:12 +0530

    Newbie

---
+++ C_Assignments/Substr/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char str[100];
+    printf("\n Enter the string \n ");
+    gets(str);
+    
+    char sub[100];
+    printf("\n Enter the substring \n");
+    gets(sub);
+    
+    int sub_str_len = strlen(sub);
+    
+    int counter = 0, j = 0;
+    for(int i = 0; str[i] != '\0'; i++){
+        if(str[i] == sub[j]){
+            counter += 1;
+            j += 1;
+            if(counter == sub_str_len){
+                printf("\n Substring exists \n");
+                return 0;
+            }
+        }
+        else{
+            counter = 0;
+            j = 0;
+        }
+    }
+    printf("\n Substring doesnt exist \n");
+    
+    return 0;
+}
