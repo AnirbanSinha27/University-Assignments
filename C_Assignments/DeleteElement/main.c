a80cc08a05e7b30b8d6730a91ae7fcfd86806de3
AnirbanSinha27 <107923453+AnirbanSinha27@users.noreply.github.com>
2023-01-10 01:20:13 +0530

    Updated main.c

--- C_Assignments/DeleteElement/main.c
+++ C_Assignments/DeleteElement/main.c
@@ -2,11 +2,14 @@
 Copyright (c) 2022 Anirban Sinha
 Licensed under MIT license
 */
+
+//This program is to delete an element form an array
 #include<stdio.h>
 int main()
 {
     int a[20],n,position;
 
+    //User input of array
     printf("Enter the number of terms: \n");
     scanf("%d",&n);
 
@@ -15,9 +18,11 @@ int main()
         scanf("%d", &a[i]);
     }
 
+    //User input of the position of the element to be deleted
     printf("Enter the position \n");
     scanf("%d",&position);
 
+    //Deleting the element
     for(int i=position-1;i<=n;i++){
         a[i]=a[i+1];
     }
