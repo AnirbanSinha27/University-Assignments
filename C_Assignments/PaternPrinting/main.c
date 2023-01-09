f017924d9f8a70aa2577ed9ef7661901967da193
AnirbanSinha27 <107923453+AnirbanSinha27@users.noreply.github.com>
2023-01-10 01:31:08 +0530

    Updated main.c

--- C_Assignments/PaternPrinting/main.c
+++ C_Assignments/PaternPrinting/main.c
@@ -2,14 +2,19 @@
 Copyright (c) 2022 Anirban Sinha
 Licensed under MIT license
 */
+
+//This program is for printing a specific pattern
 #include <stdio.h>
 
 int main()
 {
     int n;
+
+    //User input of the number of rows
     printf("Enter the value of n: \n");
     scanf("%d",&n);
 
+    //Pattern printing
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n-i;j++){
             printf(" ");
