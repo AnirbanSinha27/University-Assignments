042447db71028143888d09cd7ed3f27e65db2395
AnirbanSinha27 <107923453+AnirbanSinha27@users.noreply.github.com>
2023-01-10 01:27:57 +0530

    Updated main.c

--- C_Assignments/InsertElement/main.c
+++ C_Assignments/InsertElement/main.c
@@ -2,11 +2,14 @@
 Copyright (c) 2022 Anirban Sinha
 Licensed under MIT license
 */
+
+//This program is to insert an element into a given array
 #include<stdio.h>
 int main()
 {
     int a[20],n,position,value;
 
+    //User input of the array
     printf("Enter the number of terms: \n");
     scanf("%d",&n);
 
@@ -15,9 +18,11 @@ int main()
         scanf("%d", &a[i]);
     }
 
+    //User input of the position and value of the element to be inserted
     printf("Enter the position and value \n");
     scanf("%d %d",&position,&value);
 
+    //Inserting the element into the array
     for(int i=n-1;i>=position-1;i--){
         a[i+1]=a[i];
     }
