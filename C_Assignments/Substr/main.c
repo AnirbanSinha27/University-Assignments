ddb3f6e2730de31c9fe7cf4b0cb48ee0b83c5b01
AnirbanSinha27 <107923453+AnirbanSinha27@users.noreply.github.com>
2023-01-10 01:34:00 +0530

    Updated main.c

--- C_Assignments/Substr/main.c
+++ C_Assignments/Substr/main.c
@@ -2,20 +2,26 @@
 Copyright (c) 2022 Anirban Sinha
 Licensed under MIT license
 */
+
+//This program is to check if a substring exists within the given string
 #include <stdio.h>
 #include <string.h>
 
 int main(){
     char str[100];
+
+    //User input of the string
     printf("\n Enter the string \n ");
     gets(str);
     
+    //User input of the substring
     char sub[100];
     printf("\n Enter the substring \n");
     gets(sub);
     
     int sub_str_len = strlen(sub);
     
+    //Checking is substring exists
     int counter = 0, j = 0;
     for(int i = 0; str[i] != '\0'; i++){
         if(str[i] == sub[j]){
