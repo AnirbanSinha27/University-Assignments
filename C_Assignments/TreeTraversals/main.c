cf0d67fd55f3903b08206264b8ff627ff406c9ca
Anirban Sinha <107923453+AnirbanSinha27@users.noreply.github.com>
2023-06-30 15:05:15 +0530

    updated main.c

---
+++ C_Assignments/TreeTraversals/main.c
@@ -0,0 +1,83 @@
+/*
+Copyright (c) 2022 Anirban Sinha
+Licensed under MIT license
+*/
+
+// This program has all the tree traversals.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node* left;
+    struct node* right;
+};
+
+struct node* createNode(int data){
+    struct node *n; // creating a node pointer
+    n = (struct node *) malloc(sizeof(struct node)); // Allocating memory in the heap
+    n->data = data; // Setting the data
+    n->left = NULL; // Setting the left and right children to NULL
+    n->right = NULL; // Setting the left and right children to NULL
+    return n; // Finally returning the created node
+}
+
+//Preorder traversal
+void preOrder(struct  node* root){
+    if(root!=NULL){
+        printf("%d ", root->data);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+    return;
+}
+
+//Inorder traversal
+void inOrder(struct  node* root){
+    if(root!=NULL){
+        inOrder(root->left);
+        printf("%d ", root->data);
+        inOrder(root->right);
+    }
+    return;
+}
+
+//Postorder Traversal
+void postOrder(struct  node* root){
+    if(root!=NULL){
+        postOrder(root->left);
+        postOrder(root->right);
+        printf("%d ", root->data);
+    }
+    return;
+}
+
+int main(){
+     
+    // Constructing the root node - Using Function (Recommended)
+    struct node *p = createNode(4);
+    struct node *p1 = createNode(1);
+    struct node *p2 = createNode(6);
+    struct node *p3 = createNode(5);
+    struct node *p4 = createNode(2);
+    // Finally The tree looks like this:
+    //      4
+    //     / \
+    //    1   6
+    //   / \
+    //  5   2  
+
+    // Linking the root node with left and right children
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    preOrder(p);
+    printf("\n");
+    inOrder(p);
+    printf("\n");
+    postOrder(p);
+    return 0;
+}
